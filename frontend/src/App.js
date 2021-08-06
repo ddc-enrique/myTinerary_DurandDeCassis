77b7283f7cbe50265f3895ee1741437ef7e5849f
enrique-dc93 <51374100+enrique-dc93@users.noreply.github.com>
2021-08-06 19:25:45 -0300

    4rd commit, create Home.js, install some npm  packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {  BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="">
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
