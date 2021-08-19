34f88e47177e031e2b6c97bc71ad7b58b7a27959
enrique-dc93 <51374100+enrique-dc93@users.noreply.github.com>
2021-08-19 08:57:24 -0300

    24th commit, Itineraries firsts styles

--- frontend/src/components/Itinerary.js
+++ frontend/src/components/Itinerary.js
@@ -1,37 +1,51 @@
-import React, { useState } from 'react';
-import { Cash, Heart, HeartFill, Stopwatch } from 'react-bootstrap-icons';
+import React, { useEffect, useState } from 'react';
+import { ArrowBarDown, ArrowBarUp, Cash, Heart, HeartFill, Stopwatch } from 'react-bootstrap-icons';
+import Aos from 'aos';
+
+
 
 const Itinerary = ({itinerary}) => {
     const [ extraContent, setExtraContent] = useState(false);
+    
+    useEffect(() => {
+        Aos.init({ duration: 500 });
+    }, [])
+    
     let cash = [];
     for (let i = 1; i <= itinerary.price; i++) {
         cash.push(<Cash key={itinerary._id + "P" + i.toString() }/>)
     }
     console.log(cash);
     return (
-        <div className="itinerary">
+        <div 
+            className="itinerary"
+            data-aos="zoom-out-up"
+        >
             <div 
-                className="imageItinerary"
+                className="headerItinerary"
                 style={{backgroundImage: `url("/assets/${itinerary.src}.jpg")`}}
             >
                 <h3>{itinerary.title}</h3>
             </div>
             
-            <div className="bodyItinerary">
-                <div className="author">
-                    <div
-                        className="authorPic"
-                        style={{backgroundImage: `url("/assets/${itinerary.author.profilePic}.jpg")`}}
-                    >
-                    </div>
-                    <p>{itinerary.author.name}</p>
+            <div className="author">
+                <div
+                    className="authorPic"
+                    style={{backgroundImage: `url("/assets/${itinerary.author.profilePic}.jpg")`}}
+                >
                 </div>
-                <div className="description">
-                    <p>{itinerary.description}</p>
+                <p>{itinerary.author.name}</p>
+            </div>
+
+            <div className="bodyItinerary">
+                <div className="details">
                     <p>
                         Price: {(cash.map( moneyBill => moneyBill))}
                     </p>
                     <p><Stopwatch /> Duration: {itinerary.duration} {itinerary.duration === 1 ? "hour" : "hours"}</p>
+                </div>
+                <div className="description">
+                    <p>{itinerary.description}</p>
                 </div>
                 <div className="social">
                     <p className="likes">
@@ -58,7 +72,7 @@ const Itinerary = ({itinerary}) => {
                 className="buttonView"
                 onClick={() => setExtraContent(!extraContent)}
             >
-                View{extraContent ? " Less" : " More"}
+                View {extraContent ? " Less" : " More"} 
             </button>
         </div>
     )
